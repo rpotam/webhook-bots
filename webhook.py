18b47e234a0ba80187ba10deb00d11229fac029a
Ravi Reddy <gpr4973@gmail.com>
2020-09-30 11:25:35 +0530

    added new file

--- webhook.py
+++ webhook.py
@@ -14,11 +14,11 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
     
-    res = makeResponse(req)
+    res = processRequest(req)
     
     res = json.dumps(res, indent=4)
     # print(res)
-    r = makeResponse(res)
+    r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
@@ -27,7 +27,7 @@ def makeResponse(req):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("city")
+    city = parameters.get("geo-city")
     date = parameters.get("date")
     if city is None:
         return None
@@ -49,3 +49,20 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
