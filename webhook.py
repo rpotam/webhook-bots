3f01389c0dd133b41f68dbfe45734129aa16e64f
Ravi Reddy <gpr4973@gmail.com>
2020-09-30 11:10:53 +0530

    added webhook

--- webhook.py
+++ webhook.py
@@ -14,11 +14,11 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
     
-    res = processRequest(req)
+    res = makeResponse(req)
     
     res = json.dumps(res, indent=4)
     # print(res)
-    r = make_response(res)
+    r = makesponse(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
@@ -49,20 +49,3 @@ if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
